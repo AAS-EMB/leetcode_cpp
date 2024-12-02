6a404cb08a84f5f18a28ca4fe7a782ea0d4ae514
Anton Anosov <31388218+AAS-EMB@users.noreply.github.com>
2024-12-02 20:32:29 +0300

    [2] [Time Beats: 44.19%] [Memory Beats: 90.04%] - LeetPush

--- two_numbers/2-Add-Two-Numbers.cpp
+++ two_numbers/2-Add-Two-Numbers.cpp
@@ -12,19 +12,19 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         auto carry = 0;
-        auto temp = new ListNode;
-        auto tail = temp;
+        auto temp  = new ListNode;
+        auto tail  = temp;
 
         while (l1 or l2 or carry) {
             auto val1 = l1 ? l1->val : 0;
             auto val2 = l2 ? l2->val : 0;
 
-            auto sum = val1 + val2 + carry;
+            auto sum   = val1 + val2 + carry;
             auto digit = sum % 10;
-            carry = sum / 10;
+            carry      = sum / 10;
 
             tail->next = new ListNode(digit);
-            tail = tail->next;
+            tail       = tail->next;
 
             l1 = l1 ? l1->next : nullptr;
             l2 = l2 ? l2->next : nullptr;
