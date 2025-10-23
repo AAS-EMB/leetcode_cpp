89514d0324069a1577c2da2ceceece79d8fd6267
Anton Anosov <31388218+AAS-EMB@users.noreply.github.com>
2025-10-23 15:25:40 +0300

    [153] [Time Beats: 100.00%] [Memory Beats: 12.39%] - LeetPush

--- find-minimum-in-rotated-sorted-array/Find-Minimum-in-Rotated-Sorted-Array.cpp
+++ find-minimum-in-rotated-sorted-array/Find-Minimum-in-Rotated-Sorted-Array.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        return binarySearch(nums);
-    }
+        int left = 0;
+        int right = nums.size() - 1;
 
-private:
-    int binarySearch(vector<int> &nums) {
-        if(nums.size() == 1) return nums[0];
+        while (right > left)
+        {
+            int mid = (left + right) / 2;
 
-        if(nums.size() == 2) return nums[0] > nums[1] ? nums[1] : nums[0];
-
-        int left = 0, right = nums.size() - 1;
-        int rotation = 0;
-
-        while (left <= right) {
-            if (nums[0] <= nums[right]) {
-                return (rotation == 0 and right not_eq left) ? nums[rotation] : nums[rotation + 1];
-            } else {
-                rotation = --right;
+            if (nums[right] >= nums[mid])
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
             }
         }
 
-        return -1;
+        return nums[left];
     }
 };
