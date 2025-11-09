12ba204009c779b979ba98e740fb078360bc5a61
Anton Anosov <31388218+AAS-EMB@users.noreply.github.com>
2025-11-09 18:15:52 +0300

    [2169] [Time Beats: 100.00%] [Memory Beats: 71.74%] - LeetPush

---
+++ count-operations-to-obtain-zero/Count-Operations-to-Obtain-Zero.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int result = 0;
+
+        while (num1 and num2)
+        {
+            ++result;
+            if (num1 >= num2)
+            {
+                num1 -= num2;
+            }
+            else
+            {
+                num2 -= num1;
+            }
+        }
+
+        return result;
+    }
+};
